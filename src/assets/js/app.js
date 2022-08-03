a5dcc5e7853c866ea1695d97dc694a893a0c43d3
hanomag123 <zhenyaglibko@gmail.com>
2022-08-03 18:01:18 +0300

    adaptive

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,8 +3,54 @@ document.addEventListener("DOMContentLoaded", () => {
 
 //= components/
 
+function mainObserver() {
+    function vdHandler(els) {
+        els.forEach((data) => {
+        if (data.isIntersecting === true) {
+            data.target.classList.add('observe-active')
+        } else {
+            data.target.classList.remove('observe-active')
+        }
+        })
+    }
+    
+    const observer = new IntersectionObserver(vdHandler)
+    const observeTarget = document.querySelectorAll('.observe, .observe-left')
+    
+    observeTarget.forEach(el => {
+        observer.observe(el)
+    })
+}
 
+function preLoad() {
+    function vdHandler(els) {
+        els.forEach((data) => {
+        if (data.isIntersecting === true && Boolean(data.target.dataset.src)) {
+            data.target.src = data.target.dataset.src
+        }
+        })
+    }
+    
+    const observer = new IntersectionObserver(vdHandler)
+    const observeTarget = document.images
+    console.log(document.images)
+    for(let el of observeTarget) {
+        observer.observe(el)
+    }
+}
 
+function loadMainImg() {
+    function loadImg() {
+        const homeImg = document.querySelector('.home__bg')
+        homeImg.classList.remove('blur')
+        homeImg.src = this.src
+    }
+
+    const mainImg = document.createElement('img')
+    mainImg.src = './assets/images/main/mainBg.png'
+
+    mainImg.addEventListener('load', loadImg)
+}
 
 const swiper = new Swiper(".topSales__catalog", {
     slidesPerView: 5,
@@ -34,6 +80,10 @@ const newsSwiper = new Swiper(".news__swiper", {
     centeredSlides: true,
     reverseDirection: false,
 });
+
+mainObserver()
+preLoad()
+loadMainImg()
 })
 
 
