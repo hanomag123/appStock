3e3923ab9d1e69b0d641509a6bed51091f007ec8
KonstantinVolodko <volodko2000@bk.ru>
2022-08-16 14:54:08 +0300

    228commit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -391,6 +391,7 @@ let acc = document.getElementsByClassName("accordion");
 let i;
 
 for (i = 0; i < acc.length; i++) {
+   
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
@@ -402,21 +403,30 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+acc[0].click()
 
 
 let acc2 = document.getElementsByClassName("accordion2");
 
-for (let i = 0; i < acc2.length; i++) {
-  acc2[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
+    for (let i = 0; i < acc2.length; i++) {
+        acc2[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          
+          var panel = this.nextElementSibling;
+          if (panel.style.maxHeight){
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+          }
+        });
+      }
+
+
+
+
+
+
+
 
 
 let filterIcoContainerMobile = document.querySelector('.filterIcoContainerMobile')
